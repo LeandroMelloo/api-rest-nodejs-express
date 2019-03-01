327cd7146c4a2740864710cef34f3471e9d82f7f
LeandroMelloo <leandromoreno1909@hotmail.com>
2019-03-01 10:26:57 -0300

    ajuste de codigo

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+
+// Iniciando o App
+const app = express();
+
+// envie dados no formato Json
+app.use(express.json());
+
+// cors disponibiliza acesso a API para ser acessada publicamente
+app.use(cors());
+
+// Iniciando DB
+mongoose.connect("mongodb://localhost:27017/produto", {  useNewUrlParser: true , 
+useCreateIndex: true });
+require("./model/produto");
+
+// DEFININDO A APLICAÇÃO ==============================
+// definindo local de arquivos públicos
+app.use(express.static(__dirname + '/public'));
+// logando todas as requisições no console
+app.use(logger('dev'));
+// parse application/x-www-form-urlencoded                                    
+app.use(bodyParser.urlencoded({'extended':'true'}));
+// parse application/json          
+app.use(bodyParser.json());
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+// ROTAS ===============================================
+// Incluindo nossas rotas definidas no arquivo routes/index.js
+var index = require('./index');
+// definindo nossas rotas na aplicação
+app.use('/', index);
+
+app.listen(3000);
---
+++ controller/produtoController.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const produto = mongoose.model('Produto');
+
+// faz o exports para o serviço "rotas.js"
+module.exports = {
+    //rota de listagem dos produtos no BD
+    async index(req, res) {
+        const { page = 1 } = req.query; // desestruturação utilizado no JS6
+        const produtoIndex = await produto.paginate({}, { page, limit: 10 });
+
+        // só será executado depois que todos os registros for retornados do DB
+        return res.json(produtoIndex);
+    },
+    
+    // rota de busca dos produtos no BD
+    async show(req, res) {
+        const produtoShow = await produto.findById(req.params.id);
+
+        // só será executado depois que os registros for atualizado no DB
+        return res.json(produtoShow);
+    },
+
+    // rota de criação dos produtos no DB
+    async create(req, res) {
+        const produtoCreate = await produto.create({
+            nome : req.body.nome,
+            email : req.body.email,
+            telefone : req.body.telefone,
+            done : false
+        });
+
+        // só será executado depois que os registros for inseridos no DB
+        return res.json(produtoCreate);
+    },
+
+    // rota de atualização dos produtos no DB
+    async update(req, res) {
+        const produtoUpdate = await produto.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        // só será executado depois que os registros for inseridos no DB
+        return res.json(produtoUpdate);
+    },
+
+    // rota de exclusão dos produtos no DB
+    async delete(req, res) {
+        await produto.findByIdAndRemove(req.params.id);
+
+        // só será executado depois que os registros for inseridos no DB
+        return res.send("Produto excluido com Sucesso!");
+    },
+
+};
